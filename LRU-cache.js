4726e80ddd8742d38517886a7eee81934a57ca48
Abhiram Vadlapatla <v.abhiram97@gmail.com>
2021-06-08 23:27:24 +0530

    Create LRU-cache.js

---
+++ LRU-cache.js
@@ -0,0 +1,41 @@
+/**
+push the recently used (either by get or put methods) value in the bottom of iterator object by deleting and setting that value again. 
+**/
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.map = new Map();
+    this.capacity = capacity;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(!this.map.has(key)) return -1;
+    let value = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, value)
+    return value;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if(this.map.has(key)) this.map.delete(key);
+    this.map.set(key, value);
+    if (this.map.size > this.capacity) this.map.delete(this.map.keys().next().value);
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
